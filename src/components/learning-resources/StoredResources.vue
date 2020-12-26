218b63ed3f2a0985f60f06fbda2a7fd0369c8e6e
Dmytro_Klymchuk <dmytro.klymchuk@smile-ukraine.com>
2020-12-26 16:02:39 +0200

    initial commit

---
+++ src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <learning-resource
+        v-for="res in resources"
+        :title="res.title"
+        :id="res.id"
+        :description="res.description"
+        :link="res.link"
+        :key="res.id">
+    </learning-resource>
+  </ul>
+</template>
+
+<script>
+  import LearningResource from "./LearningResource";
+  export default {
+    components: {
+      LearningResource
+    },
+    inject: ['resources']
+  }
+
+</script>
+
+<style scoped>
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0 auto;
+    max-width: 40rem;
+  }
+</style>
